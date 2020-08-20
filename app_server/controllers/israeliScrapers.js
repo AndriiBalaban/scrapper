b65a7d17e97b29578bfeef7e705f717ac9bbdd51
AndrewB <balaban.andrew89@gmail.com>
2020-08-20 23:47:18 +0300

    cal and max

--- app_server/controllers/israeliScrapers.js
+++ app_server/controllers/israeliScrapers.js
@@ -74,3 +74,77 @@ module.exports.otsar = function (req, res) {
         }
     })();
 }
+
+module.exports.max = function (req, res) {
+    console.log('new max connection');
+    process.env.args = ['--no-sandbox', '--disable-setuid-sandbox'];
+    let options = {
+        companyId: "otsarHahayal", // mandatory; one of 'hapoalim', 'hapoalimBeOnline', leumi', 'discount', 'mizrahi', 'otsarHahayal', 'visaCal', 'max', 'isracard', 'amex'
+        startDate: moment().startOf('month'), // the date to fetch transactions from (can't be before the minimum allowed time difference for the scraper)
+        combineInstallments: false, // if set to true, all installment transactions will be combine into the first one
+        showBrowser: false, // shows the browser while scraping, good for debugging (default false)
+        verbose: false//, // include more debug info about in the output
+        // browser : Browser, // optional option from init puppeteer browser instance outside the libary scope. you can get browser diretly from puppeteer via `puppeteer.launch()` command.
+        // executablePath: string // optional. provide a patch to local chromium to be used by puppeteer. Relevant when using `israeli-bank-scrapers-core` library
+    }
+
+    const credentials = {
+        username: req.query.username,
+        password: req.query.password
+    };
+
+    (async function () {
+        try {
+            const scraper = createScraper(options);
+            const scrapeResult = await scraper.scrape(credentials);
+
+            if (scrapeResult.success) {
+                res.json(scrapeResult.accounts);
+                scrapeResult.accounts.forEach((account) => {
+                    console.log(`found ${account.txns.length} transactions for account number ${account.accountNumber}`);
+                });
+            } else {
+                throw new Error(scrapeResult.errorType);
+            }
+        } catch (e) {
+            console.error(`scraping failed for the following reason: ${e.message}`);
+        }
+    })();
+}
+
+module.exports.cal = function (req, res) {
+    console.log('new cal connection');
+    process.env.args = ['--no-sandbox', '--disable-setuid-sandbox'];
+    let options = {
+        companyId: "otsarHahayal", // mandatory; one of 'hapoalim', 'hapoalimBeOnline', leumi', 'discount', 'mizrahi', 'otsarHahayal', 'visaCal', 'max', 'isracard', 'amex'
+        startDate: moment().startOf('month'), // the date to fetch transactions from (can't be before the minimum allowed time difference for the scraper)
+        combineInstallments: false, // if set to true, all installment transactions will be combine into the first one
+        showBrowser: false, // shows the browser while scraping, good for debugging (default false)
+        verbose: false//, // include more debug info about in the output
+        // browser : Browser, // optional option from init puppeteer browser instance outside the libary scope. you can get browser diretly from puppeteer via `puppeteer.launch()` command.
+        // executablePath: string // optional. provide a patch to local chromium to be used by puppeteer. Relevant when using `israeli-bank-scrapers-core` library
+    }
+
+    const credentials = {
+        username: req.query.username,
+        password: req.query.password
+    };
+
+    (async function () {
+        try {
+            const scraper = createScraper(options);
+            const scrapeResult = await scraper.scrape(credentials);
+
+            if (scrapeResult.success) {
+                res.json(scrapeResult.accounts);
+                scrapeResult.accounts.forEach((account) => {
+                    console.log(`found ${account.txns.length} transactions for account number ${account.accountNumber}`);
+                });
+            } else {
+                throw new Error(scrapeResult.errorType);
+            }
+        } catch (e) {
+            console.error(`scraping failed for the following reason: ${e.message}`);
+        }
+    })();
+}
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,5 +4,7 @@ const ctrlIsraelScrappers = require("../controllers/israeliScrapers")
 
 router.get("/scrapper/api/leumi", ctrlIsraelScrappers.leumi);
 router.get("/scrapper/api/otsar", ctrlIsraelScrappers.otsar);
+router.get("/scrapper/api/max", ctrlIsraelScrappers.max);
+router.get("/scrapper/api/cal", ctrlIsraelScrappers.cal);
 
 module.exports = router;
