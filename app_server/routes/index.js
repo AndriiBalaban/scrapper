de47eaa0c214c5d717f016fda4ed5abdb7290a43
AndrewB <balaban.andrew89@gmail.com>
2020-08-23 23:01:47 +0300

    get to post

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const ctrlIsraelScrappers = require("../controllers/israeliScrapers")
 
-router.get("/scrapper/api/leumi", ctrlIsraelScrappers.leumi);
-router.get("/scrapper/api/otsar", ctrlIsraelScrappers.otsar);
-router.get("/scrapper/api/max", ctrlIsraelScrappers.max);
-router.get("/scrapper/api/cal", ctrlIsraelScrappers.cal);
+router.post("/scrapper/api/leumi", ctrlIsraelScrappers.leumi);
+router.post("/scrapper/api/otsar", ctrlIsraelScrappers.otsar);
+router.post("/scrapper/api/max", ctrlIsraelScrappers.max);
+router.post("/scrapper/api/cal", ctrlIsraelScrappers.cal);
 
 module.exports = router;
